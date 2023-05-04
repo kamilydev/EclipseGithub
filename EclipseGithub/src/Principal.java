7f954802ce5121a701650177422385db9426949d
Kamily <113871690+kamilydev@users.noreply.github.com>
2023-05-04 13:40:09 -0300

    correção

--- EclipseGithub/src/Principal.java
+++ EclipseGithub/src/Principal.java
@@ -2,7 +2,7 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Conexão Github e Eclipse criada xom sucesso!");
+		System.out.println("Conexão Github e Eclipse criada com sucesso!");
 
 	}
 
